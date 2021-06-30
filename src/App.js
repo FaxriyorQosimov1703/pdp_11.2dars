93283d02224cedb7175f7422e149098710ef1a02
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:32:11 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useState, useEffect} from 'react';
+import Red from './components/Red'
+import Green from './components/Green'
+import Yellow from './components/Yellow'
+
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+
+  function App() {
+    const [time, setTime] = useState(20);
+
+    useEffect(() => {
+      let Time = setInterval(() => {
+        if(time === 0) {
+          setTime(20);
+        }else {
+          setTime(prevT => prevT - 1)
+        }
+      }, 1000)
+      return () => clearInterval(Time)
+    }, [time])
+
+    return (
+      <div className="wrapper">
+        <div>
+          <div className="box">
+            <div className="round">{10<time?<Red />:''}</div>
+            <div className="round">{time < 14 && time > 10? <Yellow />:''}</div>
+            <div className="round">{time<11?<Green />:''}</div>
+            <h1>{time}</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
 
 export default App;
